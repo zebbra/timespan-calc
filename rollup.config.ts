00acf9f31d39a040b8b0a03a4b4e446255d18453
Hannes Wüthrich <hannes.wuethrich@zebbra.ch>
2019-08-29 16:56:03 +0200

    Use yarn

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'sla-calculator'
+const libraryName = 'timespan-calc'
 
 export default {
   input: `src/index.ts`,
